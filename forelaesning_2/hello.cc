f5530023a34d7b9c83a45f2c9714e746aa6130c7
Louise Ravn <louiseravn@Louises-MacBook-Air.local>
2026-01-28 11:15:13 +0100

    first commit

---
+++ forelaesning_2/hello.cc
@@ -0,0 +1,11 @@
+#include"hello.hpp"
+#include<iostream>
+
+
+void hello(void){
+    std::cout << "hello from hello" << "\n";
+}
+
+
+
+
---
+++ forelaesning_2/main.cc
@@ -0,0 +1,9 @@
+
+#include"hello.hpp"
+
+#include<iostream>
+int main(){
+//	std::cout << "Hello, World!" << std::endl;         Dont want main to do actual computation
+	hello();
+	return 0;
+}
