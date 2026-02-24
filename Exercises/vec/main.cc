ad08afbe7c7bb3889e462ccd8ec611c2b1163d9f
Louise Ravn <louiseravn@Louises-MacBook-Air.local>
2026-02-24 07:21:22 +0100

    complex numbers

--- Exercises/vec/main.cc
+++ Exercises/vec/main.cc
@@ -8,28 +8,27 @@ int main(){
     std::mt19937 gen(0);
 	std::uniform_real_distribution<double> dist(-1.0, 1.0);
 	
-	vec::vec u,v,test;
+	vec::vec<double> u,v,test;
 	u.set(dist(gen),dist(gen),dist(gen));
 	v.set(dist(gen),dist(gen),dist(gen));
-	u.print(" u= ");
-	v.print(" v= ");
-	std::cout << " u=" << u << std::endl;
-	std::cout << " v=" << v << std::endl;
+	u.print(" random vector u= ");
+	v.print("\n random vector v= ");
+	
 
 	test.set(-u.x,-u.y,-u.z);
 	(-u).print("-u  = ");
-	test.print("test= ");
-	if(approx(-u,test))std::cout << "\ntest 'unary -' passed\n" << std::endl;
+	test.print("\n test= ");
+	if(approx(-u,test))std::cout << "\ntest '-' passed\n" << std::endl;
 
 	test.set(u.x+v.x,u.y+v.y,u.z+v.z);
 	(u+v).print(" u+v = ");
-	test.print (" test= ");
-	if(approx(u+v,test))std::cout << " test '+' passed\n" << std::endl;
+	test.print ("\n test= ");
+	if(approx(u+v,test))std::cout << "\n test '+' passed\n" << std::endl;
 
 	test.set(u.x-v.x,u.y-v.y,u.z-v.z);
 	(u-v).print("u-v = ");
-	test.print   ("test= ");
-	if(approx(u-v,test))std::cout << " test '-' passed\n" << std::endl;
+	test.print   ("\n test= ");
+	if(approx(u-v,test))std::cout << "\n test '-' passed\n" << std::endl;
 
 
 
@@ -37,12 +36,12 @@ int main(){
 
 	test.set(u.x*c,u.y*c,u.z*c);
 	(u*c).print("u*c  = ");
-    test.print (" test= ");
+    test.print ("\n test= ");
     if(vec::approx(u*c,test))std::cout << "\ntest '*' passed\n" << std::endl;
 	
     double t = u.x*v.x + u.y*v.y + u.z*v.z;
 	std::cout << "dot(u,v)=" << dot(u,v)<< std::endl;
-    std::cout << " test= " << t << std::endl;
+    std::cout << " \n test= " << t << std::endl;
     if(vec::approx(dot(u,v),t))std::cout << "\ntest dot passed\n" << std::endl;
 
 
@@ -51,6 +50,23 @@ int main(){
     std::cout << "The old had to define a new vector 'r' manually: vec r = a;\n";
     std::cout << "This adds an extra line (higher chance for typos).\n";
     std::cout << " Instead, can simply edit the copy of the first vector. \n";
+
+
+	std::cout << "\n Testing with comlex number:" << "\n";
+
+
+	using cplex = std::complex<double>;
+
+	// creating vectora
+	vec::vec<cplex> uc(cplex(1, 1), 2.0, 0.0);
+	vec::vec<cplex> vc(cplex(0, 1), 1.0, 0.0);
+
+	// Addition: (1+i, 2, 0) + (0+i, 1, 0) = (1+2i, 3, 0)
+	(uc + vc).print("uc + vc = "); 
+
+	// Norm: |1+i|^2 = 2, |2|^2 = 4, |0|^2 = 0. Sum = 6. Norm = sqrt(6) ≈ 2.449
+	std::cout << "\nNorm(uc) [Expect 2.449]: " << vec::norm(uc);
+
 
 
 return 0;
--- Exercises/vec/vec.h
+++ Exercises/vec/vec.h
@@ -1,48 +1,153 @@
 #pragma once
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<complex>
 
 namespace vec{
+
+    template <typename T>
     struct vec {
-        double x, y, z;
+        T x, y, z;
 
         // constructors
-        vec(double x, double y, double z) : x(x), y(y), z(z) {}  // parameterized
+        vec(T x, T y, T z) : x(x), y(y), z(z) {}  // parameterized
         vec() : vec(0, 0, 0) {}                                    // default
-        vec(const vec&) = default;                                 // copy
-        vec(vec&&) = default;                                      // move
+        vec(const vec<T>&) = default;                                 // copy
+        vec(vec<T>&&) = default;                                      // move
         ~vec() = default;                                          // destructor
 
         // assignment
-        vec& operator=(const vec&) = default;                     // copy assignment
-        vec& operator=(vec&&) = default;                          // move assignment
+        vec<T>& operator=(const vec<T>&) = default;                     // copy assignment
+        vec<T>& operator=(vec<T>&&) = default;                          // move assignment
 
-        // arithmetic
-        vec& operator+=(const vec&);
-        vec& operator-=(const vec&);
-        vec& operator*=(double);
-        vec& operator/=(double);
+        // arithmetics
+        vec<T>& operator+=(const vec<T>& a){
+            x += a.x;
+            y += a.y;
+            z += a.z;
+            return *this;
+        }
+        
+        
+        vec<T>& operator-=(const vec<T>& a){
+            x -= a.x;
+            y -= a.y;
+            z -= a.z;
+            return *this;
+        }
+
+     
+        vec<T>& operator*=(T a){
+            x *= a;
+            y *= a;
+            z *= a;
+            return *this;
+        }
+       
+        vec<T>& operator/=(T a){
+            x /= a;
+            y /= a;
+            z /= a;
+            return *this;
+        }
+
 
         // utility
-        void set(double a, double b, double c);
-        void print(const std::string& s = "") const;              // for debugging
+        void set(T a, T b, T c) { x = a; y = b; z = c; }
+
+        
+        void print(const std::string& s) const{
+            std::cout << s << x << " " << y << " " << z;
+        }           
 
         // stream output
-        friend std::ostream& operator<<(std::ostream&, const vec&);
+        friend std::ostream& operator<<(std::ostream& os, const vec<T>& v) {
+            os << v.x << " " << v.y << " " << v.z;
+            return os;
+        }
     };
 
-    // non-member operators
-    vec operator-(const vec&);
-    vec operator-(const vec&, const vec&);
-    vec operator+(vec, const vec&);
-    vec operator*(const vec&, double);
-    vec operator*(double, const vec&);
-    vec operator/(const vec&, double);
-    double dot(const vec&, const vec&);
-    vec cross(const vec&, const vec&);
-    double norm(const vec&);
+   
+
+
+  
+
+
+    template <typename T>
+    vec<T> operator+(vec<T> a, const vec<T>& b){
+        a += b;
+        return a;
+    }
+
+    template <typename T>
+    vec<T> operator-(const vec<T>& a){
+	vec r=a;
+	r.x=-r.x;
+	r.y=-r.y;
+	r.z=-r.z;
+	return r;
+    }
+
+    template <typename T>
+    vec<T> operator-(const vec<T>& a, const vec<T>& b){
+        vec<T> r=a;
+        r-=b;
+        return r;
+    }
+
+    template <typename T>
+    vec<T> operator*(const vec<T>& a, T n){
+        vec<T> r=a;
+        r*=n;
+        return r;
+    }
+
+    template <typename T>
+    vec<T> operator*(T n, const vec<T>& a){
+        vec<T> r=a;
+        r*=n;
+        return r;
+    }
+
+
+    // dot cross norm
+
+     template <typename T>
+    T dot(const vec<T>& a, const vec<T>& b){
+        return a.x*b.x + a.y*b.y + a.z*b.z;
+    }
+
+    template <typename T>
+    vec<T> cross(const vec<T>& a, const vec<T>& b) {
+
+        vec<T> r;
+        r.x = a.y*b.z - a.z*b.y;
+        r.y = a.z*b.x - a.x*b.z;
+        r.z = a.x*b.y - a.y*b.x;
+
+        return r;
+    }
+
+    template <typename T>
+    auto norm(const vec<T>& a) {
+        return std::sqrt(std::norm(a.x) + std::norm(a.y) + std::norm(a.z));
+    }
 
     // approximate equality
-    bool approx(double a,double b,double acc=1e-6,double eps=1e-6);
-    bool approx(const vec&, const vec&, double acc = 1e-6, double eps = 1e-6);
+    template <typename T>
+    bool approx(T a,T b,double acc = 1e-6 ,double eps = 1e-6){
+        using std::abs;
+        auto diff = abs(a-b);
+	    if( diff <acc)return true;
+	    if( diff <eps*(abs(a)+ abs(b)))return true;
+	    return false;
+	}
+    template <typename T>
+    bool approx(const vec<T>& u, const vec<T>& v, double acc =1e-6, double eps = 1e-6){
+        auto d = norm(u - v);
+        if (d < acc) return true;
+        if (d < eps * (norm(u) + norm(v))) return true;
+        return false;
+	}
 }
