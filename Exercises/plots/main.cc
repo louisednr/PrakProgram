d6e38b03bfae56b7f897d5fe9116e08c0d448634
Louise Ravn <louiseravn@Louises-MacBook-Air.local>
2026-02-25 13:49:56 +0100

    plots

---
+++ Exercises/plots/main.cc
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<iomanip>
+#include<vector>
+#include<numbers>
+
+constexpr double NaN = std::numeric_limits<double>::quiet_NaN();
+constexpr double PI = std::numbers::pi;
+
+
+double my_erf(double x){
+    // single precision error function (Abramowitz and Stegun, from Wikipedia)
+    if(x<0) return -erf(-x);
+    std::vector<double> a {0.254829592,-0.284496736,1.421413741,-1.453152027,1.061405429};
+    double t=1/(1+0.3275911*x);
+    double sum=t*(a[0]+t*(a[1]+t*(a[2]+t*(a[3]+t*a[4]))));/* the right thing */
+    return 1-sum*std::exp(-x*x);
+} 
+
+
+
+double sgamma(double x){
+    if(x<0)return PI/std::sin(PI*x)/sgamma(1-x);
+    if(x<9)return sgamma(x+1)/x;
+    double lnsgamma=std::log(2*PI)/2+(x-0.5)*std::log(x)-x
+        +(1.0/12)/x-(1.0/360)/(x*x*x)+(1.0/1260)/(x*x*x*x*x);
+    return std::exp(lnsgamma);
+}
+
+
+double lngamma(double x){
+    if(x<=0) return NaN;
+    if(x<9) return lngamma(x+1)-log(x);
+    return x*std::log(x+1/(12*x-1/x/10))-x+std::log(2*PI/x)/2;
+}
+
+
+
+
+int main(int argc, char** argv) {
+    double xmin =  -5, xmax=  5, dx = 0.0001;
+    std::string mode = "erf"; // default mode
+
+    for (int i = 0; i < argc; i++) {
+        std::string arg = argv[i];
+        if (arg == "-xmin" && i + 1 < argc) xmin = std::stod(argv[i + 1]);
+        if (arg == "-xmax" && i + 1 < argc) xmax = std::stod(argv[i + 1]);
+        if (arg == "-dx"   && i + 1 < argc) dx = std::stod(argv[i + 1]);
+        if (arg == "-mode" && i + 1 < argc) mode = argv[i + 1];
+    }
+
+    std::cout << std::scientific;
+
+    if (mode == "erf") {
+        for (double x = xmin; x <= xmax; x += dx) {
+            std::cout << x << " " << my_erf(x) << "\n";
+        }
+    }
+    else if (mode == "gamma") {
+        for (double x = xmin; x <= xmax; x += dx) {
+            std::cout << x << " " << sgamma(x) << "\n";
+        }
+
+    } else if (mode == "lngamma") {
+        for (double x = xmin; x <= xmax; x += dx) {
+            std::cout << x << " " << lngamma(x) << "\n";
+        }
+    }
+
+
+
+    return 0;
+}
+
+
+
+
