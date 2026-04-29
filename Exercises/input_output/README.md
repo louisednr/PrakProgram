db8d1e33707fa116b58b87b535f25d124bc1f173
Louise Ravn <louiseravn@Louises-MacBook-Air.local>
2026-04-29 15:05:22 +0200

    added readme

---
+++ Exercises/input_output/README.md
@@ -0,0 +1,13 @@
+# C++ Input/Output Exercise
+
+To compile the code and execute all three exercise tasks (Command-line, Standard Input, and File Streams) simultaneously, run:
+
+```bash
+make
+```
+
+
+To remove the binary and all generated .txt files:
+```bash
+make clean
+```
--- Exercises/input_output/main.cc
+++ Exercises/input_output/main.cc
@@ -1,53 +1,55 @@
 #include<iostream>
 #include<vector>
 #include<fstream>
-
+#include <cmath>
 
 
 int main (int argc, char* argv[]) {
 
-    std::string infile="", outfile="";
-    std::vector<double> numbers;
+    std::vector<double> n_vals;
+    std::string infile = "", outfile = "";
 
-
-
-	for(int i=0;i<argc;++i){
-		std::string arg=argv[i];
-		if(arg=="-n" && i+1<argc)
-			numbers.push_back(std::stod(argv[i+1]));
-	}
-    for(auto n: numbers)
-	    std::cout << n <<" "<< std::sin(n) <<" "<< std::cos(n) <<std::endl;
-
-
-
-
-    double x;
-    while( std::cin >> x ){
-	    std::cout << x <<" "<< std::sin(x) <<" "<< std::cos(x) << std::endl;
-	}
-
-
-    for(int i=0;i<argc;i++){
-		std::string arg=argv[i];
-		if(arg=="--input" && i+1 < argc) infile=argv[i+1];
-		if(arg=="--output" && i+1 < argc) outfile=argv[i+1];
-	}
-    std::ifstream myinput(infile);
-    std::ofstream myoutput(outfile);
-
-
-    if( myinput.is_open() && myoutput.is_open() ){
-        std::cout << "3";
-	    while( myinput >> x ){
-		myoutput << x <<" "<<std::sin(x)<<" "<<std::cos(x)<<std::endl;
-		}
-	}
-    else{
-	    std::cerr << "Error opening files: " << infile << outfile << std::endl;
-	    return EXIT_FAILURE;
+	// read all arguments
+	for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if (arg == "-n" && i + 1 < argc) {
+            n_vals.push_back(std::stod(argv[++i]));
+        } else if (arg == "--input" && i + 1 < argc) {
+            infile = argv[++i];
+        } else if (arg == "--output" && i + 1 < argc) {
+            outfile = argv[++i];
+        }
     }
-myinput.close();
-myoutput.close();
-exit(EXIT_SUCCESS);
+
+
+	if (!infile.empty() && !outfile.empty()) {
+        std::ifstream input(infile);
+        std::ofstream output(outfile);
+        double x;
+        if (input.is_open() && output.is_open()) {
+            while (input >> x) {
+                output << x << " " << std::sin(x) << " " << std::cos(x) << std::endl;
+            }
+        }
+    } 
+
+
+	else if (!n_vals.empty()) {
+        for (double n : n_vals) {
+            std::cout << n << " " << std::sin(n) << " " << std::cos(n) << std::endl;
+        }
+    } 
+  
+    else {
+        double x;
+        while (std::cin >> x) {
+            std::cout << x << " " << std::sin(x) << " " << std::cos(x) << std::endl;
+        }
+    }
+
+    return 0;
+
 }
+
+
+
